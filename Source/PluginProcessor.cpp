adac2d7b397b97453c559566b5ce8d5463f8dfc6
Simon Fay <simonjohnfay@gmail.com>
2023-06-20 14:15:47 +0100

    first commit

---
+++ Source/PluginProcessor.cpp
@@ -0,0 +1,217 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+Sjf_karplusStrongAudioProcessor::Sjf_karplusStrongAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+                     #endif
+                       )
+#endif
+{
+    m_string.prepare( getSampleRate() );
+}
+
+Sjf_karplusStrongAudioProcessor::~Sjf_karplusStrongAudioProcessor()
+{
+}
+
+//==============================================================================
+const juce::String Sjf_karplusStrongAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool Sjf_karplusStrongAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool Sjf_karplusStrongAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool Sjf_karplusStrongAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double Sjf_karplusStrongAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int Sjf_karplusStrongAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int Sjf_karplusStrongAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void Sjf_karplusStrongAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const juce::String Sjf_karplusStrongAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void Sjf_karplusStrongAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
+
+//==============================================================================
+void Sjf_karplusStrongAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    m_string.prepare( sampleRate );
+}
+
+void Sjf_karplusStrongAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool Sjf_karplusStrongAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    juce::ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    // Some plugin hosts, such as certain GarageBand versions, will only
+    // load plugins that support stereo bus layouts.
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void Sjf_karplusStrongAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+{
+    juce::ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+
+    auto bufferSize = buffer.getNumSamples();
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+
+//    for ( auto index = 0; index < bufferSize; index++ )
+//    {
+//        if( midiMessages )
+//    }
+    
+    auto noteIndex = 0;
+    auto checkMidi = false;
+    std::vector< std::array< int, 3 > > noteList;
+    auto listIndex = 0;
+    for ( const auto event : midiMessages )
+    {
+        if ( event.getMessage().isNoteOn() )
+        {
+            auto pos = event.samplePosition;
+            auto pitch = event.getMessage().getNoteNumber();
+            auto vel = static_cast< int >(event.getMessage().getVelocity());
+            noteList.push_back( { pos, pitch, vel } );
+        }
+    }
+    if ( noteList.size() > 0 )
+    {
+        checkMidi = true;
+        noteIndex = noteList[ listIndex ][ 0 ];
+    }
+    
+    for ( auto indexThroughCurrentBuffer = 0; indexThroughCurrentBuffer < bufferSize; indexThroughCurrentBuffer++ )
+    {
+        while ( checkMidi && ( indexThroughCurrentBuffer == noteIndex ) && ( listIndex < noteList.size() ) )
+        {
+//            auto noteRef = *note;
+            DBG( noteList[ listIndex ][ 1 ] << " " << noteList[ listIndex ][ 2 ] );
+            m_string.triggerNewNote( noteList[ listIndex ][ 1 ], noteList[ listIndex ][ 2 ] );
+            listIndex += 1;
+            if( listIndex >= noteList.size() )
+                break;
+            noteIndex = noteList[ listIndex ][ 0 ];
+        }
+        
+//        m_string.processSample( indexThroughCurrentBuffer );
+    }
+
+    
+    
+}
+
+//==============================================================================
+bool Sjf_karplusStrongAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+juce::AudioProcessorEditor* Sjf_karplusStrongAudioProcessor::createEditor()
+{
+    return new Sjf_karplusStrongAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void Sjf_karplusStrongAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void Sjf_karplusStrongAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new Sjf_karplusStrongAudioProcessor();
+}
---
+++ Source/PluginProcessor.h
@@ -0,0 +1,64 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "../sjf_audio/sjf_karplusStrong.h"
+//==============================================================================
+/**
+*/
+class Sjf_karplusStrongAudioProcessor  : public juce::AudioProcessor
+                            #if JucePlugin_Enable_ARA
+                             , public juce::AudioProcessorARAExtension
+                            #endif
+{
+public:
+    //==============================================================================
+    Sjf_karplusStrongAudioProcessor();
+    ~Sjf_karplusStrongAudioProcessor() override;
+
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+
+   #ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+   #endif
+
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    //==============================================================================
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    //==============================================================================
+    const juce::String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    //==============================================================================
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const juce::String getProgramName (int index) override;
+    void changeProgramName (int index, const juce::String& newName) override;
+
+    //==============================================================================
+    void getStateInformation (juce::MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+private:
+    //==============================================================================
+    
+    sjf_karplusStrongVoice m_string;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Sjf_karplusStrongAudioProcessor)
+};
